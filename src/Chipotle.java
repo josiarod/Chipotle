dfa074b1e405f972116b28e235b3217eb8b31c85
GBTC441011UR <GBTC441011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-31 09:11:06 -0500

    initial commit

---
+++ src/Chipotle.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Chipotle {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        String[] rice = {"white","brown","none","all"};
+        String[] meat = {"chicken","steak","carnidas","chorizo","sofritas","veggies","none","all"};
+        String[] beans = {"pinto","black","none"};
+        String[] veggies ={"lettuce","fajita veggies","none","all"};
+        String[] cheese = {"yes","no"};
+        String[] queeso = {"yes","no"};
+        String[] sourCreeam = {"yes", "no"};
+
+        for(int i = 0; i < 25; i++){
+
+        }
+    }
+}
